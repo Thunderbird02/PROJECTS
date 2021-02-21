4e133f159b8fe383b742a154d0a653c173b81313
batsi <batsi.nyama@gmail.com>
2021-02-21 17:53:16 +0200

    winner

---
+++ ONE_STOP_SHOP/javascript/main.js
@@ -0,0 +1,9 @@
+//get buttons
+// we can put them in an array 
+let men_button   = document.getElementById("men_button");
+let women_button = document.getElementById("women_button");
+let Babies_button= document.getElementById("Babies_button");
+let Kids_button  = document.getElementById("Kids_button");
+let Scholars_button = document.getElementById("Scholars_button");
+let beauty_button  = document.getElementById("beauty_button");
+
