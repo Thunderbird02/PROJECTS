987dd4780949b3bc1a17897b1d7afb8072e1704f
batsi <batsi.nyama@gmail.com>
2021-04-04 19:56:16 +0200

    First Second

--- SCRAPER/apis_in_action.py
+++ SCRAPER/apis_in_action.py
@@ -18,34 +18,79 @@ def github_search(search_term):
     repo_descr = repo_list.find_all('p' , class_ = 'mb-1')
     #extract languages 
     stars = repo_list.find_all('a' , class_ ='Link--muted' )
+    #footer containing many elements
+    footer = repo_list.find_all('div' , class_ = 'd-flex flex-wrap text-small color-text-secondary')
+    
 
-    lang = repo_list.find_all('span' , {'itemprop' : True })
+    manipulate_repo_list(repo_list , repo_descr , stars  , footer)
 
-    manipulate_repo_list(repo_list , repo_descr , stars , lang)
+def manipulate_repo_list(repo_list , repo_descr , stars , footer ):
 
-def manipulate_repo_list(repo_list , repo_descr , stars  , lang):
-
-     index = 0   
-     description = repo_list.find_all('a' , class_ = 'v-align-middle')
-     divs_tags = []
+    index = 0   
+    description = repo_list.find_all('a' , class_ = 'v-align-middle')
+    divs_tags = []
   
-     for item in description:
-         repo_names.append(item.text)
+    for item in description:
+        repo_names.append(item.text)
 
-     for repo_desc_item in repo_descr:
-         repo_description.append(repo_desc_item.text.strip())
+    for repo_desc_item in repo_descr:
+        repo_description.append(repo_desc_item.text.strip())
 
-     for x in stars :
-         if len(x.get('class')) == 2:
-             continue
-         else:
-             star_s.append(x.text)
+    for x in stars :
+        if len(x.get('class')) == 2:
+            continue
+        else:
+            star_s.append(x.text)
 
-     for language in lang :
-         print(language.text)
+    print()
+    print('Licence starts ')
+    #Licence 
+    for ITEM in footer :
+        if len(ITEM.findChildren('div' , recursive = False)) >= 3:
+   
+            Children = ITEM.findChildren('div' , recursive = False)
+                         
+       
+    print('language starts')
+    #Language
+    for Item in footer :
+        if len(Item.findChildren('div' , recursive = False)) >= 3:
+            children = Item.find_all('div' , class_ ='mr-3')
 
-         
+            for child in children:
+                language = child.find_all('span' , {'itemprop': True})
+                if len(language) != 0:
+                    print(language[0].text)
+        else:
+            print('none ' +  ' ')
+
+    #language has been debugged it works nicely         
+
+    print()
+    print('Number_Of_Issues')
+    #Number_Of_Issues
+    for _item in footer :
+        if len(_item.findChildren('div' , recursive = False)) >= 3:
+            number_of_issues = item.find('a' , class_ = 'Link--muted f6')
+            print(number_of_issues.text)
+            print()
+        else:
+            print(' none ' + ' ')
+
+        print()
+        print('Last Updated')
+       #Last Updated 
+    for item_ in footer :
+        date_  = item_.find('relative-time' , class_ = 'no-wrap')
+        print('Updated on ' + date_.text )
+
+             
 if __name__ == "__main__":
     search_term = input("'Enter keyword(s)[e.g python, flask, postgres]:")
     github_search(search_term)
+
+
+
+
+
 
